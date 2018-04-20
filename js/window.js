4641093f181a0fe9e0cb76e4b1055a8ac7a48faa
pavlivvp86 <pavliv.vp@gmail.com>
2018-04-21 00:10:30 +0300

    First release

---
+++ js/window.js
@@ -0,0 +1,5 @@
+function openGoogle(){
+	var win = window.open('http://www.google.com/', 'googleWindow', 'width=400,height=400,left=10,top=10');
+		win.resizeTo(700,300);
+		win.moveTo(100,500);
+};
